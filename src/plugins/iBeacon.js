7cf8380eb704b3b80e2c692186e0f92b64d43130
Hasan Dogu Taskiran <hdtaskiran@locopass.com>
2015-06-25 20:46:34 -0700

    Fix didChangeAuthorizationStatus

--- src/plugins/iBeacon.js
+++ src/plugins/iBeacon.js
@@ -50,9 +50,9 @@ angular.module('ngCordova.plugins.iBeacon', [])
           });
         };
     
-        delegate.didChangeAuthorizationStatus = function (status) {
+        delegate.didChangeAuthorizationStatus = function (pluginResult) {
           $timeout(function () {
-            $rootScope.$broadcast('$cordovaiBeacon:didChangeAuthorizationStatus', status);
+            $rootScope.$broadcast('$cordovaiBeacon:didChangeAuthorizationStatus', pluginResult);
           });
         };
    
